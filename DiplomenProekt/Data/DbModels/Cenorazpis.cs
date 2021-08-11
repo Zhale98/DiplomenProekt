8c4209af20c4d3922986254263e37801bb562c20
Jale <Jale@DESKTOP-HU97ROM>
2021-08-11 19:22:38 +0300

    frontend

---
+++ DiplomenProekt/Data/DbModels/Cenorazpis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiplomenProekt.Data.DbModels
+{
+    public class Cenorazpis
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Categoriq { get; set; }
+        public int Dni { get; set; }
+        public double Cena { get; set; }
+        public string Smqna { get; set; }
+    }
+}
---
+++ DiplomenProekt/Data/DbModels/Smeni.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiplomenProekt.Data.DbModels
+{
+    public class Smeni
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Smqna { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
